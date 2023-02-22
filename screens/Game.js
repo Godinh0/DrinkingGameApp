f5875676ca2dd1b6fbcd90091d04413203bfcd78
Godinh0 <gabriel_godinho@outlook.com>
2023-02-22 20:02:19 -0300

    Basic Mecanic

--- screens/Game.js
+++ screens/Game.js
@@ -1,16 +1,30 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import React, { useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
-const Game = (cards) => {
+const Game = () => {
   const navigation = useNavigation();
 
-  const [RandomCard,setRandomCard] = useState(1);
+  const cards = [
+    require("./carta1.png"),
+    require("./carta2.png"),
+    require("./carta3.png"),
+    require("./carta4.png"),
+    require("./carta5.png"),
+  ];
 
-  function CardHandle(cards) {
-    const random = (min,max) => Math.floor(Math.random()* (max- min) +min )
-    setRandomCard(random(0,10));
-  };
+  const [RandomCard, setRandomCard] = useState();
+
+  function CardHandle() {
+    const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
+    setRandomCard(cards[random(0, 5)]);
+  }
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -19,15 +33,22 @@ const Game = (cards) => {
   }, []);
 
   return (
-    <SafeAreaView className="flex-1 bg-[#ef3537] items-center ">
-      <View className= "items-center">
+    <SafeAreaView className="flex-1 bg-[#ef3537] ">
+      <View className="items-center">
         <TouchableOpacity className="  mt-20 w-30" onPress={CardHandle}>
           <Text className="font-bold text-xl  border-2 rounded-full p-2 bg-white">
             New Card
           </Text>
         </TouchableOpacity>
-          <Text className="font-bold text-5xl w-30 mt-96" >{RandomCard}</Text>
+        <Image className="w-80 h-80 mt-20" source={RandomCard} />
       </View>
+        <View className="absolute bottom-10  w-30 h-25  ml-36">
+          <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+            <Text className="font-bold text-xl  border-2 rounded-full p-2 bg-white">
+              End Game
+            </Text>
+          </TouchableOpacity>
+        </View>
     </SafeAreaView>
   );
 };
